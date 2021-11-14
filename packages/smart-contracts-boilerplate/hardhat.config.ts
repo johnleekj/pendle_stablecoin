9737be2ba90da31732720ea95de90444e8cb4dde
johnleekj <43510466+johnleekj@users.noreply.github.com>
2021-11-14 12:27:48 +0800

    gas reporter added but buggy

--- packages/smart-contracts-boilerplate/hardhat.config.ts
+++ packages/smart-contracts-boilerplate/hardhat.config.ts
@@ -1,6 +1,9 @@
 import { task, HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
 import "solidity-coverage";
+import "hardhat-gas-reporter";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -32,6 +35,13 @@ const config: HardhatUserConfig = {
   },
   mocha: {
     timeout: 20000,
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    showMethodSig: true,
+    onlyCalledMethods: false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
 };
 
