08393f9971ce0b58e7d2dee79001a9b7d4c5da13
johnleekj <43510466+johnleekj@users.noreply.github.com>
2021-11-25 12:06:27 +0800

    build: pulled changes from chee yang

---
+++ node_modules/smart-contracts-boilerplate/contracts/KhooleeCoinMinter.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+//import '@boringcrypto/boring-solidity/contracts/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+// Vault Contract will deploy this contract and be the minter
+contract KhooleeCoinMinter is ERC20PresetMinterPauser {
+    constructor() ERC20PresetMinterPauser('KhooleeCoin', 'KLC') {
+    }    
+    
+    
+  
+}
