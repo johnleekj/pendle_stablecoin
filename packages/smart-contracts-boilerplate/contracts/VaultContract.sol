bbbb070968411e12afe419012c982d1c0628d94d
sleepypanda <cheeyang85@gmail.com>
2021-11-25 11:36:39 +0800

    build: OT is valued at 0.8usd now. Flat fee will be incurred each time you add collateral. Debt ceiling is added. KLC can now be repayed in exchange for the deposited collateral.

--- packages/smart-contracts-boilerplate/contracts/VaultContract.sol
+++ packages/smart-contracts-boilerplate/contracts/VaultContract.sol
@@ -9,6 +9,12 @@ import './KhooleeCoinMinter.sol';
 contract KhooleeCoinMinterInterface {
     function mint(address to, uint256 amount) public virtual {
     }
+
+    function balanceOf(address owner) public view returns (uint256) {
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+    }
 }
 
 // need to create an interface for MockOT to facilitate 
@@ -42,21 +48,17 @@ contract VaultContract {
     uint256 private constant LOAN_TO_VALUE_PRECISION = 1e2; // 100
     
     uint256 private constant LIQUIDATION_THRESHOLD = 75; // 75% (percentage at which a position is defined as under-collateralized)
+    // collaterization rate is 100% if position is defined as under-collateralized
     uint256 private constant LIQUIDATION_THRESHOLD_PRECISION = 1e2; // 100
     
-    
-    //uint256 private constant INTEREST_PER_SECOND = 317097920;
+    // this is incurred each time collateral is being added
+    uint256 private constant FLAT_FEE = 20; // 20 usd worth of mockOT
 
-    //uint256 private constant COLLATERIZATION_RATE = 75000; // 75%
-    //uint256 private constant COLLATERIZATION_RATE_PRECISION = 1e5; // Must be less than EXCHANGE_RATE_PRECISION (due to optimization in math)
+    // this is not being used for now
+    uint256 private constant INTEREST_PER_MONTH = 4; // 4%
+    uint256 private constant INTEREST_PER_MONTH_precision = 1e2; // 100 
 
-    //uint256 private constant EXCHANGE_RATE_PRECISION = 1e18;
-
-    //uint256 private constant LIQUIDATION_MULTIPLIER = 112000; // add 12%
-    //uint256 private constant LIQUIDATION_MULTIPLIER_PRECISION = 1e5;
-
-    //uint256 private constant BORROW_OPENING_FEE = 50; // 0.05%
-    //uint256 private constant BORROW_OPENING_FEE_PRECISION = 1e5;
+    uint256 private constant DEBT_CEILING = 20; // max amount of KLC that can be borrowed at any time
     
      // Total amounts
     uint256 public totalCollateral; // Total collateral supplied
@@ -69,49 +71,31 @@ contract VaultContract {
     KhooleeCoinMinter khooleeCoinMinterContract;
     MockOTInterface mockOTContract;
 
+    address public KhooleeCoinMinterContractAddress;
     
     constructor() {
         khooleeCoinMinterContract = new KhooleeCoinMinter();
+        KhooleeCoinMinterContractAddress = address(khooleeCoinMinterContract);
     }
-    
+
     function SetMockOTContract(address addr) public {
         mockOTContract = MockOTInterface(addr);
     }
     
-    // Functions for MockOT
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        return mockOTContract.transfer(recipient, amount);
-    }
     
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        return mockOTContract.transferFrom(sender, recipient, amount);
-    }    
-    
-    function balanceOf(address owner) public view returns (uint256) {
-        return mockOTContract.balanceOf(owner);
-    }
-    
-    function getStart() external view returns (uint256){
-        return mockOTContract.getStart();
-    }
-
-    function getExpiry() external view returns (uint256){
-        return mockOTContract.getExpiry();
-    } 
-    // --------------------------------------
-    
-    // to use this, user contract needs to approve this first
+    // to use this, user contract needs to approve VaultContract first
+    // flat fee will be incurred each time u add collateral
     function addCollateral(uint256 collateralAmount) public {
-        require(balanceOf(msg.sender) >= collateralAmount);
-        transferFrom(msg.sender, address(this),  collateralAmount);
-        //mockOTContract.transferFrom(msg.sender, address(this),  collateralAmount);
+        uint256 flatFeeInMockOT = FLAT_FEE * 10 / getMockOtUSDValue(); 
+        require(mockOTContract.balanceOf(msg.sender) >= collateralAmount + flatFeeInMockOT);
+        mockOTContract.transferFrom(msg.sender, address(this),  collateralAmount + flatFeeInMockOT);
         userCollateral[msg.sender] = userCollateral[msg.sender].add(collateralAmount);
         totalCollateral = totalCollateral.add(collateralAmount);
     }
 
     function removeCollateral(uint256 collateralAmount) public {
         require(collateralAmount <= userCollateral[msg.sender]);
-        transfer(msg.sender, collateralAmount);
+        mockOTContract.transfer(msg.sender, collateralAmount);
         userCollateral[msg.sender] = userCollateral[msg.sender].sub(collateralAmount);
         totalCollateral = totalCollateral.sub(collateralAmount);
         
@@ -120,14 +104,26 @@ contract VaultContract {
 
     function borrowKhooleeCoins(uint256 borrowAmount) public {
         // borrowed KhooleeCoins will be newly minted 
-        uint256 maxAmountThatCanBeBorrowed = userCollateral[msg.sender] * LOAN_TO_VALUE / LOAN_TO_VALUE_PRECISION;
+        uint256 maxAmountThatCanBeBorrowed = userCollateral[msg.sender] * getMockOtUSDValue() * LOAN_TO_VALUE / LOAN_TO_VALUE_PRECISION / 10;
         require(borrowAmount + userBorrowed[msg.sender] <=  maxAmountThatCanBeBorrowed);
-        
+        require(totalBorrow + borrowAmount + userBorrowed[msg.sender] <= DEBT_CEILING);
+
         khooleeCoinMinterContract.mint(msg.sender, borrowAmount); // mints to the borrower
         userBorrowed[msg.sender] = userBorrowed[msg.sender].add(borrowAmount);
         totalBorrow = totalBorrow.add(borrowAmount);
     }    
 
+    function repayDebt() public {
+        require(khooleeCoinMinterContract.balanceOf(msg.sender) >= userBorrowed[msg.sender]);
+        uint256 borrowAmount = userBorrowed[msg.sender];
+
+        // repayed stablecoins will be burnt
+        khooleeCoinMinterContract.burnFrom(msg.sender ,userBorrowed[msg.sender]);
+        userBorrowed[msg.sender] = 0;
+        totalBorrow = totalBorrow.sub(borrowAmount);
+
+        removeCollateral(userCollateral[msg.sender]);
+    }
 
     function liquidate_when_liquidatable_debt_position(address addressToLiquidate) private {
         // if health factor < 100, can liquidate collateral
@@ -150,15 +146,12 @@ contract VaultContract {
     // else, collateral still safe
     function returnHealthFactor() public view returns(uint256) {
         if (userBorrowed[msg.sender] == 0) return 100;
-        
-        
-        uint256 a = percent(userCollateral[msg.sender] * percent(LIQUIDATION_THRESHOLD, LIQUIDATION_THRESHOLD_PRECISION, 2), userBorrowed[msg.sender] * 100 , 2);
-
+        uint256 a = percent(userCollateral[msg.sender] * getMockOtUSDValue() * percent(LIQUIDATION_THRESHOLD, LIQUIDATION_THRESHOLD_PRECISION, 2), userBorrowed[msg.sender] * 100 , 2) / 10;
         return a;
         //return ((userCollateral[msg.sender] * LIQUIDATION_THRESHOLD / LIQUIDATION_THRESHOLD_PRECISION) / userBorrowed[msg.sender] *);
     }
     
-    function percent(uint numerator, uint denominator, uint precision) public pure returns(uint quotient) {
+    function percent(uint numerator, uint denominator, uint precision) private pure returns(uint quotient) {
         // caution, check safe-to-multiply here
         uint _numerator  = numerator * 10 ** (precision+1);
         // with rounding of last digit
@@ -166,6 +159,14 @@ contract VaultContract {
         return ( _quotient);
     }
 
+    // supposed to be oracle to get value, but this will do for now
+    function getMockOtUSDValue() private pure returns (uint256) {
+        return percent(8, 10, 1);
+    } 
+
+    function getDebtCeiling() public view returns (uint256) {
+        return DEBT_CEILING;
+    }
     
     // for testing purposes
     function returnCurrentMsgSender() external view returns (address){
